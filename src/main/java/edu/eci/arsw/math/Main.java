73c815a422e7cf17cb8a92770fc221396e2083e6
unknown <kevin.cuitiva-p@labinfo.is.escuelaing.edu.co>
2026-09-04 11:39:31 -0500

    actualizacion cantidad

--- src/main/java/edu/eci/arsw/math/Main.java
+++ src/main/java/edu/eci/arsw/math/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static void main(String a[]) {
         System.out.println(bytesToHex(PiDigits.getDigits(0, 10)));
         System.out.println(bytesToHex(PiDigits.getDigits(1, 100)));
-        System.out.println(bytesToHex(PiDigits.getDigits(1, 1000000)));
+        System.out.println(bytesToHex(PiDigits.getDigits(1, 10000)));
     }
 
     private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
